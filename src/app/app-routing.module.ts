3964923041d9ea8af30cca092fa0b32353c531df
Alejandro Pasmin Vega <alejandro.pasmin@correounivalle.edu.co>
2021-12-14 14:08:59 -0500

    Login y Usuarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientesComponent } from './clientes/clientes.component';
+import { LoginComponent } from './login/login.component';
 import { PortadaComponent } from './portada/portada.component';
 import { ProductosComponent } from './productos/productos.component';
 import { ReportesComponent }from './reportes/reportes.component' 
@@ -11,7 +12,10 @@ const routes: Routes = [{path: 'home', component: PortadaComponent},
 {path: 'Reportes', component: ReportesComponent},
 {path: '', component: PortadaComponent},
 {path: 'Ventas', component: VentasComponent},
-{path: 'ManejoProductos', component: ProductosComponent}];
+{path: 'ManejoProductos', component: ProductosComponent},
+{path: 'Login', component: LoginComponent},
+{path: 'CrearUsuarios', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PortadaComponent } from './portada/portada.component';
 import { ReportesComponent } from './reportes/reportes.component';
 import { VentasComponent } from './ventas/ventas.component';
 import { ProductosComponent } from './productos/productos.component';
+import { CrearUsuariosComponent } from './crear-usuarios/crear-usuarios.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { ProductosComponent } from './productos/productos.component';
     PortadaComponent,
     ReportesComponent,
     VentasComponent,
-    ProductosComponent
+    ProductosComponent,
+    CrearUsuariosComponent
   ],
   imports: [
     BrowserModule,
