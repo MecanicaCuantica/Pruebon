b13d821c565e9ac030e1dc63d44a1a7f8d893db5
Alejandro Pasmin Vega <alejandro.pasmin@correounivalle.edu.co>
2021-12-14 19:53:45 -0500

    Edits-Removes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientesComponent } from './clientes/clientes.component';
+import { CrearUsuariosComponent } from './crear-usuarios/crear-usuarios.component';
 import { LoginComponent } from './login/login.component';
 import { PortadaComponent } from './portada/portada.component';
 import { ProductosComponent } from './productos/productos.component';
@@ -14,7 +15,7 @@ const routes: Routes = [{path: 'home', component: PortadaComponent},
 {path: 'Ventas', component: VentasComponent},
 {path: 'ManejoProductos', component: ProductosComponent},
 {path: 'Login', component: LoginComponent},
-{path: 'CrearUsuarios', component: LoginComponent}
+{path: 'CrearUsuarios', component: CrearUsuariosComponent}
 ];
 
 @NgModule({
