59cb2c36933ed7ca6a7e94cf382a52763b99b25e
Cristian Chaparro <cristian.chaparro@correounivalle.edu.co>
2021-12-15 14:30:54 -0500

    interfaz usuario y crud-usuarios

--- src/app/usuarios-crud/usuarios-crud.component.ts
+++ src/app/usuarios-crud/usuarios-crud.component.ts
@@ -1,4 +1,8 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UsuariosService } from '../Services/usuarios.service';
 
 @Component({
   selector: 'app-usuarios-crud',
@@ -7,9 +11,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosCRUDComponent implements OnInit {
 
-  constructor() { }
+  CreateUsuario: FormGroup;
+  submitted = false;
+  loading = false;
+  id: string | null;
+  titulo = 'Agregar Usuario';
+
+  constructor(
+    private Usuarioservice: UsuariosService,
+    private fb: FormBuilder,
+    private router: Router,
+    private aRoute: ActivatedRoute) {
+    this.CreateUsuario = this.fb.group({
+      Nombre: ['', Validators.required],
+      Cedula: ['', Validators.required],
+      Email: ['', Validators.required],
+      Direccion: ['', Validators.required],
+      Telefono: ['', Validators.required],
+      Apellido: ['', Validators.required],
+      Ocupacion: ['', Validators.required],
+      Usuario: ['', Validators.required],
+      Contrasena: ['', Validators.required]
+
+    })
+    this.id = this.aRoute.snapshot.paramMap.get('id');
+    console.log(this.id)
+
+
+  }
 
   ngOnInit(): void {
   }
 
+  agregarEditarUsuario() {
+    this.submitted = true;
+    console.log(this.CreateUsuario)
+    if(this.CreateUsuario.invalid){
+      return;
+    }
+    const Usuario: any = {
+      Nombre: this.CreateUsuario.value.Nombre,
+      Cedula: this.CreateUsuario.value.Cedula,
+      Email: this.CreateUsuario.value.Email,
+      Direccion: this.CreateUsuario.value.Direccion,
+      Telefono: this.CreateUsuario.value.Telefono,
+      Apellido: this.CreateUsuario.value.Apellido,
+      Ocupacion: this.CreateUsuario.value.Ocupacion,
+      Usuario: this.CreateUsuario.value.Usuario,
+      Contrasena: this.CreateUsuario.value.Contrasena,
+    }
+    this.Usuarioservice.agregarUsuario(Usuario).then(() =>{
+      console.log("Usuario Llego");
+      this.router.navigate(['/Usuarios'])
+    }).catch(error => {
+      console.log(error);
+    })
+  }
+
+  agregarEmpleado() {
+   
+    
+  }
+
+  editarEmpleado(id: string) {
+
+ 
+  }
+
+
+  esEditar() {
+    
+    if(this.id !== null){
+      this.titulo = 'Editar Usuario'
+      this.Usuarioservice.getUsuario(this.id).subscribe(data => {
+        this.CreateUsuario.setValue({
+        Nombre: [data.payload.data()['Nombre']],
+        Cedula: [data.payload.data()['Cedula']],
+        Email: [data.payload.data()['Email']],
+        Direccion: [data.payload.data()['Direccion']],
+        Telefono: [data.payload.data()['Telefono']],
+        Apellido: [data.payload.data()['Apellido']],
+        Ocupacion: [data.payload.data()['Ocupacion']],
+        Usuario: [data.payload.data()['Usuario']],
+        Contrasena: [data.payload.data()['Contrasena']]  
+
+        })
+      })
+    }
+
+  }
 }
+
